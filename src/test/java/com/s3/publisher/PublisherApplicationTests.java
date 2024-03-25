84f666bbb9a5e6956ace731b2f2b7c3e74b824a3
Sebastián Catalán Hermosilla <scatalanhermosilla@gmail.com>
2024-03-25 10:23:16 -0300

    first commit

---
+++ src/test/java/com/s3/publisher/PublisherApplicationTests.java
@@ -0,0 +1,13 @@
+package com.s3.publisher;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PublisherApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
